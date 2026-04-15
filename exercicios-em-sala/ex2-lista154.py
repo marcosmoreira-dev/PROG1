f9ea078bfe6d19db725a3f7110466b2964517e21
Marcos Vinícius Moreira da Silva <marcos.dev26@gmail.com>
2026-04-15 20:41:29 -0300

    Add unique elements filter for a list
    Implement a program to filter unique elements from a list.

---
+++ exercicios-em-sala/ex2-lista154.py
@@ -0,0 +1,9 @@
+# Dada uma lista com elementos duplicados, como minha_lista = [1, 2, 3, 4, 4, 5, 1], escreva um programa que crie uma nova lista contendo apenas elementos únicos da lista original, sem repetições. A nova lista deve ser [1, 2, 3, 4, 5].
+
+minhaLista = [1, 2, 3, 4, 4, 5, 1]
+novaLista = []
+
+for N in minhaLista:
+    if N not in novaLista:
+        novaLista.append(n)
+print(novaLista)
