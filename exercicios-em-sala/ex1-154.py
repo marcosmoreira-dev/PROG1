c2323254e99cf4250e5a43249b94b5b1daab858f
Marcos Vinícius Moreira da Silva <marcos.dev26@gmail.com>
2026-04-15 20:20:57 -0300

    Adiciona script para filtrar números maiores que 10
    Implementa um programa que filtra números maiores que 10.

---
+++ exercicios-em-sala/ex1-154.py
@@ -0,0 +1,9 @@
+# Dada a lista numeros = [12, -5, 23, 4, 18, 9, 27, 10, -1], escreva um programa que crie uma nova lista contendo apenas números que são maiores que 10. Ao final, imprima a nova lista.
+
+numeros = [12, -5, 23, 4, 18, 9, 27, 10, -1]
+lista = []
+
+for i in range(len(numeros)):
+    if numeros[i] > 10:
+        lista.append(numeros[i])
+print(lista)
