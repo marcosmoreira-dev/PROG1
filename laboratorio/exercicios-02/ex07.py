bf1b1a685be1bd6c4bed1131d25fa0b37c8985f8
Marcos Vinícius Moreira da Silva <marcos.dev26@gmail.com>
2026-04-24 17:56:11 -0300

    Update ex07.py

--- laboratorio/exercicios-02/ex07.py
+++ laboratorio/exercicios-02/ex07.py
@@ -1,7 +1,11 @@
 # Peça ao usuário um número inteiro N. Escreva um programa que gere e imprima os N primeiros termos da sequência de Fibonacci. A sequência começa com 0 e 1, e cada termo subsequente é a soma dos dois anteriores (Ex.: 0, 1, 1, 2, 3, 5, 8, ...)
 
-numero = int(input('Digite um número inteiro: '))
-primeiroTermo = 0
-segundoTermo = 1
+fibo = [0, 1]
+n = int(input('Digite um número: '))
 
-for i in range(numero):
+if n > 1:
+    for i in range(0, n+1):
+        fibo.append(fibo[i+1] + fibo[i])
+        print(fibo)
+else:
+    print('Digite um número válido')
