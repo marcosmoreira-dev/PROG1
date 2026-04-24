1f5379a0513e262edc91c5fe949b7e606d3ea393
Marcos Vinícius Moreira da Silva <marcos.dev26@gmail.com>
2026-04-24 12:15:19 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,7 @@
+N1 = int(input())
+N2 = int(input())
+
+print(f'{N1} é igual a {N2}: {N1 == N2}')
+print(f'{N1} é diferente de {N2}: {N1 != N2}')
+print(f'{N1} é maior que {N2}: {N1 > N2}')
+print(f'{N1} é menor ou igual a {N2}: {N1 <= N2}')
