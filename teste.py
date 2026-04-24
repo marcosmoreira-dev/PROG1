bc852da15cb23012e7c34f16b0cc7277d1c8836e
Marcos Vinícius Moreira da Silva <marcos.dev26@gmail.com>
2026-04-24 12:21:25 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
-N1 = int(input())
-N2 = int(input())
+senhaSecreta = 'programar'
+senhaUsuario = input('Digite a senha: ')
 
-print(f'{N1} é igual a {N2}: {N1 == N2}')
-print(f'{N1} é diferente de {N2}: {N1 != N2}')
-print(f'{N1} é maior que {N2}: {N1 > N2}')
-print(f'{N1} é menor ou igual a {N2}: {N1 <= N2}')
+if senhaSecreta == senhaUsuario: 
+    print(True)
+else:
+    print(False)
