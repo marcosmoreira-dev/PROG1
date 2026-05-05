0a417847a33defad9ec146cc1025b7c2a3cc42f5
Marcos Vinícius Moreira da Silva <marcosvi321@gmail.com>
2026-05-04 21:05:43 -0300

    adiciona dois exercicios da aula de strings do slide

---
+++ laboratorio/slide-strings/ex001.py
@@ -0,0 +1,12 @@
+frase = 'python é uma linguagem de programação'
+frase1 = frase.title()
+frase2 = frase1.strip()
+frase3 = "".join(frase2)
+fraseFinal = "#" + frase3
+
+# fraseFinal = # + "".join(frase.title().split())
+#
+#
+
+
+
