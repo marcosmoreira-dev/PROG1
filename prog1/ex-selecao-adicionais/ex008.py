be583060e058228d2740f0d82d2fe321f3fbb343
Marcos Vinícius Moreira da Silva <marcos.dev26@gmail.com>
2026-05-05 10:57:50 -0300

    Create ex008.py

---
+++ prog1/ex-selecao-adicionais/ex008.py
@@ -0,0 +1,4 @@
+salario = float(input('Digite o valor do salário mínimo: '))
+horas = int(input('Digite o número de horas trabalhadas: '))
+dependentes = int(input('Digite o número de dependentes: '))
+horasExtras = int(input('Digite o número de horas extras trabalhadas: '))
