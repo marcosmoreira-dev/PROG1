dc197dc62c7c61e5c82dfd68bc48a13295c9a40d
Marcos Vinícius Moreira da Silva <marcos.dev26@gmail.com>
2026-05-18 23:48:26 -0300

    Update JogoDaVelha.py

--- laboratorio/JogoDaVelha.py
+++ laboratorio/JogoDaVelha.py
@@ -1,3 +1,7 @@
+
+# Dúvidas
+# Como limpar a tela?
+
 # Cria a matriz que representa o tabuleiro
 velha = [
     [' ', ' ', ' '],
@@ -5,35 +9,66 @@ velha = [
     [' ', ' ', ' ']
 ]
 
+# Cria uma função que cria um cabecalho
+def cabecalho():
+    print(' Bem Vindo ao Jogo da Velha ')
+    print('---------TABULEIRO ---------')
+    print('\n')
+
+# Cria uma função que organiza o jogo
+def organizaJogo():
+# Laço aninhado para percorrer a matriz (linhas e colunas)
+# O laço externo pega cada linha
+    for l in range(len(velha)):
+        # O laço interno pega cada célula da linha atual
+        for c in range(len(velha[l])):
+            print(f' {velha[l][c] }', end=" | ")
+        # Pula uma linha para a próxima fileira do tabuleiro
+        print('\n---------------')
+
+# Cria uma função que "limpa a tela"
+def limpaTela():
+    # Adiciona 100 linhas
+    print('\n' * 100)
+
 vencedor = False
 jogadas = 0
 vez = 'X'
 
-print(' Bem Vindo ao Jogo da Velha ')
-print(' ---- TABULEIRO ----')
+cabecalho()
 
 # Laço aninhado para percorrer a matriz (linhas e colunas)
 # O laço externo pega cada linha
-for l in range(len(velha)):
-    # O laço interno pega cada célula da linha atual
-    for c in range(len(velha[l])):
-        print(f' {velha[l][c] }', end=" | ")
-    # Pula uma linha para a próxima fileira do tabuleiro
-    print('\n---------------')
+organizaJogo()
 
 # Cria um laço While para permanecer efetuando enquanto não existe um vencedor e o número de jogadas é menor ou igual a 9.
 while vencedor != True and jogadas <= 9:
     print(f'É a vez do jogador: {vez}')
 
     # Pede o input para saber em qual linha e coluna o jogador deseja efetuar a jogada
-    linha = int(input('Insira a linha em que seja jogar: '))
+    linha = int(input('Insira a linha em que deseja jogar: '))
     coluna = int(input('Insira a coluna em que deseja jogar: '))
 
-    # Insere o valor definido no lugar correto
-    if vez == 'X':
-        velha[linha][coluna] = 'X'
-        print(velha)
-    elif vez == 'O':
-        velha[linha][coluna] = 'O'
-        print(velha)
+    if velha[linha][coluna] == ' ':
+        # Insere o valor definido no lugar correto
+        # Caso seja a vez do jogador X
+        if vez == 'X':
+            velha[linha][coluna] = 'X'
+            jogadas += 1
+            vez = 'O'
+        # Caso seja a vez do jogador O
+        elif vez == 'O':
+            velha[linha][coluna] = 'O'
+            vez = 'X'
+            jogadas += 1
+        limpaTela()
+        cabecalho()
+        organizaJogo()
+        
+    # CONSERTAR ESSA PARTE DO IF E ELSE PARA SABER QUANDO JÁ ESTÁ OCUPADO A POSIÇÃO
+    elif velha[linha][coluna] == 'X' or velha[linha][coluna] == 'O':
+        cabecalho()
+        organizaJogo() 
+        print('A jogada é inválida')
+        print('Jogue novamente')
     
