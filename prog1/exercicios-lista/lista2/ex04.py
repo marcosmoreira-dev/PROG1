ee4fbddbdc3e913a0bf92dee528601dda4c19e83
Marcos Vinícius Moreira da Silva <marcos.dev26@gmail.com>
2026-06-13 13:23:50 -0300

    modifica exercicios de prog 1

--- prog1/exercicios-lista/lista2/ex04.py
+++ prog1/exercicios-lista/lista2/ex04.py
@@ -1,1 +1,35 @@
 # Faça um programa em Python para ler, por meio de uma função, uma lista A de elementos inteiros, escreva uma função que gere (crie uma nova) uma lista B cujos componentes são os fatoriais dos respectivos componentes de A. Imprima as listas A e B.
+
+def ler_lista(nome):
+    lista = []
+    n = int(input(f'Digite o número de elemendos da {nome}: '))
+    for i in range(n):
+        valor = int(input(f'{nome}[{i}]: '))
+        lista.append(valor)
+    return lista
+
+def fatorial(n):
+    resultado = 1
+    i = 1
+    while i <= n:
+        resultado *= i
+        i += 1
+    return resultado
+
+def gerar_lista_fatoriais(lista_a):
+    lista_b = []
+    for i in range(len(lista_a)):
+        fat = fatorial(lista_a[i])
+        lista_b.append(fat)
+    return lista_b
+
+lista_a = ler_lista('Lista A')
+lista_b = gerar_lista_fatoriais(lista_a)
+
+print('\n Lista A:')
+for i in range(len(lista_a)):
+    print(lista_a[i])
+
+print('\n Lista B (fatoriais de A):')
+for i in range(len(lista_b)):
+    print(lista_b[i])
--- prog1/exercicios-lista/lista2/ex05.py
+++ prog1/exercicios-lista/lista2/ex05.py
@@ -1,1 +1,31 @@
 # Escreva um programa em Python que leia uma lista e posteriormente faça a rotação de seus elementos. Imprima a lista antes e depois da rotação.
+
+def ler_lista():
+    lista = []
+    n = int(input('Digite o número de elementos da lista: '))
+    for i in range(n):
+        valor = int(input(f'Elemento {i}: '))
+        lista.append(valor)
+    return lista
+
+def rotacionar_lista(lista):
+    n = len(lista)
+    i = 0
+    while i < n // 2:
+        temp = lista[i]
+        lista[i] = lista[n - 1 - i]
+        lista[n - 1 - i] = temp
+        i += 1
+
+# Programa principal
+
+lista = ler_lista()
+print('\nLista original: ')
+for i in range(len(lista)):
+    print(lista[i])
+
+rotacionar_lista(lista)
+
+print('\nLista após rotação:')
+for i in range(len(lista)):
+    print(lista[i])
